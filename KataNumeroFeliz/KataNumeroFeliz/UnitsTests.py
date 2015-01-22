28a1f610579c4bc4464574264d29dfe96888c008
francalles <francalles@gmail.com>
2015-01-22 19:10:10 +0100

    Empiezo de nuevo validando el numero

--- KataNumeroFeliz/KataNumeroFeliz/UnitsTests.py
+++ KataNumeroFeliz/KataNumeroFeliz/UnitsTests.py
@@ -2,7 +2,23 @@ import unittest
 from KataNumeroFeliz import *
 
 class Test_KataNumeroFeliz(unittest.TestCase):
-    def test_CuadradoDeUnNumero(self):
-        operaciones = Operaciones()
-        result = operaciones.GetCuadrado(2)
-        self.assertEqual(4,result)
+
+    def test_EsNumeroNatural(self):
+        validacion = Validaciones()
+        resutl = validacion.GetNumeroValido()
+        self.assertTrue(result)
+    
+    #def test_CuadradoDeUnNumero(self):
+    #    operaciones = Operaciones()
+    #    result = operaciones.GetCuadrado(2)
+    #    self.assertEqual(4,result)
+    
+    #def test_SumaCuadradosDigitos(sefl):
+    #    operaciones = Operaciones()
+    #    result = operaciones.GetSuma
+
+        
+
+
+if __name__ == '__main__':
+unittest.main()
