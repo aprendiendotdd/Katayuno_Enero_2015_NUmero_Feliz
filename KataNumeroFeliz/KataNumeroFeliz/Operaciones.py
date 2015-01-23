cab4c6800e77b7935f94b04e17d52c6471e39684
francalles <francalles@gmail.com>
2015-01-23 10:28:30 +0100

    Fin

--- KataNumeroFeliz/KataNumeroFeliz/Operaciones.py
+++ KataNumeroFeliz/KataNumeroFeliz/Operaciones.py
@@ -2,12 +2,20 @@ class Operaciones():
     def GetCuadrado(self, numero):
         return numero * numero
 
-    def GetSumaCuadrados(self, numero):
+    def GetSumaCuadrados(self, numero):      
         total = 0  
         while (self.GetDigitos(numero)):                             
             total += self.GetCuadrado(self.GetResto(numero))
             numero = self.GetDivisionEntera(numero)
         return total + self.GetCuadrado(numero)
+
+      #recusividad
+      #if (self.GetDigitos(numero) == 0):
+      #      return total + self.GetCuadrado(numero)
+      #  else:
+      #      total += self.GetCuadrado(self.GetResto(numero))
+      #      self.GetSumaCuadrados(self.GetDivisionEntera(numero), total)
+
 
     def GetDigitos(self,numero):
         return (self.GetDivisionEntera(numero) != 0)
